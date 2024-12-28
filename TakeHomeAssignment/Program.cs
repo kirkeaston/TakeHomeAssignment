75b8323db2da94a3af68bbac2065ead304472a19
Kirk Easton <kirkeaston@MacBookPro.bad>
2024-12-27 18:19:27 -0700

    added basic reading of the file

--- TakeHomeAssignment/Program.cs
+++ TakeHomeAssignment/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using FuzzySharp;
 
-Console.WriteLine("Hello, World!");
+namespace TakeHomeAssignment;
+class Program
+{
+    private static string _filePath;
+
+    // Takes a .txt file of strings separated by a new line and returns potential duplicates
+    static void Main(string[] args)
+    {
+        
+        //read the file
+        Console.WriteLine("Enter the file path:");
+        _filePath = Console.ReadLine();
+        
+        //returns an array of strings to be processed
+        string[] linesArray = FileReader.CreateArrayFromFile(_filePath);
+        
+        //process the lines
+        
+        //produce a file of duplicates
+        
+    }
+}
